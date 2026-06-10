287a2daab8523a9c295d8c218138426fcbde1d11
SamWlms10 <samwlms10@gmail.com>
2026-06-10 14:45:00 -0400

    Created the getComputerChoice function

--- RockPaperScissor.js
+++ RockPaperScissor.js
@@ -1,1 +1,7 @@
 // creating a function that gets the computers choice
+function getComputerChoice() {
+    const rockPaperScissor = ["Rock","Paper","Scissors"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return rockPaperScissor[randomNumber];
+}
+
